ee34436b1d88eda1203ad362000e0f43e2fb72f5
farida-chn <faridachoirun_n@student.ub.ac.id>
2025-10-17 14:27:52 +0700

    commit1_UserData

---
+++ app/src/main/java/data/UserData'.kt
@@ -0,0 +1,9 @@
+package data
+
+data class UserData(
+    val email: String = "",
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val password: String = ""
+)
